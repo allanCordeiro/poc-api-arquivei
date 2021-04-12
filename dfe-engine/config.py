97f3071f6002f434377286a471aa3a0239be0b7a
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-04-12 18:13:39 -0300

    dfe-engine: chaves e tags para get de config

--- dfe-engine/config.py
+++ dfe-engine/config.py
@@ -8,7 +8,12 @@ logging.basicConfig(filename='logs/config.txt',
 
 class LocalConfig:
     @staticmethod
-    def __get_credentials(credential_key):
+    def __open_json():
+        with open("config.json", "r") as json_file:
+            return json.load(json_file)
+
+    @staticmethod
+    def get_credentials(credential_key):
         try:
             if credential_key == 'x-api-id':
                 return config('x-api-id')
@@ -21,4 +26,13 @@ class LocalConfig:
             logging.info('A chave não foi encontrada. Verifique se o arquivo .env existe')
             logging.error(f'Exception -> {e}')
 
+    @staticmethod
+    def get_config(tag):
+        json_file = LocalConfig.__open_json()
+        return json_file[tag]
+
+    @staticmethod
+    def get_endpoint(endpoint_name):
+        json_file = LocalConfig.get_config("endpoints")
+        return json_file[endpoint_name]
 
