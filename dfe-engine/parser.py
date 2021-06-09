f42d0df15d8d9997ab9aae3bde02d63bfb34d28b
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-06-09 18:06:54 -0300

    dfe-engine: poc de parser do nfe

---
+++ dfe-engine/parser.py
@@ -0,0 +1,12 @@
+import xmltodict
+
+file_path = 'files/nfe/xml/31210411606543001811550010000136601158604209.xml'
+
+with open(file_path, "r") as file:
+    xml = file.read()
+
+doc = xmltodict.parse(xml)
+#NFE model
+print(doc["nfeProc"]["NFe"]["infNFe"]["ide"]["dhEmi"])
+print(doc["nfeProc"]["NFe"]["infNFe"]["emit"]["CNPJ"])
+print(doc["nfeProc"]["NFe"]["infNFe"]["dest"]["CNPJ"])
